b33bfceb5c71653c3bd6332c685a34ef25090ad7
wtransw <wouter.anseeuw@gmail.com>
2022-05-06 15:38:23 +0200

    Rabbit mq test endpoint in controller.

--- src/PlanningApi/Controllers/PlanningController.cs
+++ src/PlanningApi/Controllers/PlanningController.cs
@@ -1,4 +1,6 @@
 ﻿using CalendarServices;
+using CalendarServices.Models;
+using Crm.Link.RabbitMq.Producer;
 using Microsoft.AspNetCore.Mvc;
 using PlanningApi.Configuration;
 
@@ -11,16 +13,29 @@ namespace PlanningApi.Controllers
         private readonly ILogger<PlanningController> Logger;
         private readonly IGoogleCalendarService CalendarService;
         private readonly CalendarOptions CalendarOptions;
+        private readonly PlanningAttendeePublisher PlanningAttendeePublisher;
+        private readonly PlanningSessionPublisher PlanningSessionPublisher;
+        private readonly PlanningSessionAttendeePublisher PlanningSessionAttendeePublisher;
+
+        private readonly PlanningService PlanningService;
         public PlanningController(
             ILogger<PlanningController> logger, 
             IGoogleCalendarService calendarService,
-            CalendarOptions calendarOptions)
+            CalendarOptions calendarOptions,
+            PlanningAttendeePublisher planningAttendeePublisher,
+            PlanningSessionPublisher planningSessionPublisher,
+            PlanningSessionAttendeePublisher planningSessionAttendeePublisher,
+            PlanningService planningService)
         {
             this.Logger = logger;
             this.CalendarOptions = calendarOptions; 
             this.CalendarService = calendarService;
             CalendarService.CalendarGuid = calendarOptions.CalendarGuid;
             Logger.LogInformation("PlanningController created");
+            this.PlanningAttendeePublisher = planningAttendeePublisher;
+            this.PlanningSessionPublisher = planningSessionPublisher;
+            this.PlanningSessionAttendeePublisher = planningSessionAttendeePublisher;
+            this.PlanningService = planningService;
         }
 
 
@@ -44,6 +59,45 @@ namespace PlanningApi.Controllers
         {
             Logger.LogInformation("RabbitEndpoint called");
             return Ok();
+        }
+
+        [HttpPost("PublishOnRabbitMqTest")]
+        public async Task<IActionResult> PublishOnRabbitMqTest(int objectNumber)
+        {
+            object obj;
+            string testUuid = "12345678901234567890123456789012";
+            var attendee = new PlanningAttendee { Name = "Wouter", LastName = "A", Email = "my@mail.here", VatNumber = "", Version = 12 };
+            var session = new PlanningSession("eerste sessie", new DateTime(2022, 12, 01), new DateTime(2022, 12, 2), "Omschrijving van de eerste sessie", testUuid);
+            var sessionAttendee = new PlanningSessionAttendee(MethodEnum.create, testUuid, testUuid, NotificationStatus.pending);
+
+            obj = attendee;
+
+            try
+            {
+
+                switch (objectNumber)
+                {
+                    case 1:
+                        PlanningAttendeePublisher.Publish(attendee);
+                        break;
+                    case 2:
+                        PlanningSessionPublisher.Publish(session);
+                        break;
+                    case 3:
+                        PlanningSessionAttendeePublisher.Publish(sessionAttendee);
+                        break;
+                    default:
+                        break;
+                }
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError($"couldn't publish object of type number {objectNumber}: {ex.Message}");
+            }
+            return BadRequest();
+
         }
 
     }
