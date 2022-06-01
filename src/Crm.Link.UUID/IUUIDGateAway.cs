6beafe34fedebd21639e72ded4633d5aa16e9d13
wtransw <wouter.anseeuw@gmail.com>
2022-06-01 20:27:47 +0200

    aaa

--- src/Crm.Link.UUID/IUUIDGateAway.cs
+++ src/Crm.Link.UUID/IUUIDGateAway.cs
@@ -8,6 +8,6 @@ namespace Crm.Link.UUID
         Task<ResourceDto?> PublishEntity(string source, EntityTypeEnum entityType, string sourceEntityId, int version);
         Task<ResourceDto?> UpdateEntity(string id, string sourceType, EntityTypeEnum entityType);
         Task<ResourceDto?> UpdateEntity(string id, string sourceType, EntityTypeEnum entityType, int newVersion);
-        Task<ResourceDto?> GetResource(Guid id, string source)
+        Task<ResourceDto?> GetResource(Guid id, string source);
     }
 }
